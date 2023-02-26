1a1233c343e92e131a56098e059678a1c528e558
Shantanu Sutar <shantanusutar17@gmail.com>
2023-02-26 19:37:22 +0530

    Content setup and styling
    Also, fake data downloaded in mock_data json file which contains names, email, gender and ip_addresses

---
+++ app/src/App.jsx
@@ -0,0 +1,20 @@
+import "./App.css";
+import JSONDATA from "./MOCK_DATA.json";
+import { useState } from "react";
+function App() {
+  return (
+    <div className="App">
+      <input type="text" placeholder="Search" />
+
+      {JSONDATA.map((val, key) => {
+        return (
+          <div>
+            <p>{val.first_name}</p>
+          </div>
+        );
+      })}
+    </div>
+  );
+}
+
+export default App;
