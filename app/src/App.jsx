a1bebc9f9bc63bd82b15f1f81f6966a60940f74f
Shantanu Sutar <shantanusutar17@gmail.com>
2023-02-26 20:06:07 +0530

    search filter working
    we use useState() and filter property here
    
    if searchName is empty, display all names
    
    else we check if JSON value's first name contains the letters of the searchName  {  if  yes  return  the  name  }

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -2,13 +2,28 @@ import "./App.css";
 import JSONDATA from "./MOCK_DATA.json";
 import { useState } from "react";
 function App() {
+  const [searchName, setSearchName] = useState("");
   return (
     <div className="App">
-      <input type="text" placeholder="Search" />
-
-      {JSONDATA.map((val, key) => {
+      <input
+        type="text"
+        placeholder="Search..."
+        onChange={(e) => {
+          setSearchName(e.target.value);
+        }}
+      />
+      {console.log(searchName)}
+      {JSONDATA.filter((val, key) => {
+        if (searchName === "") {
+          return val;
+        } else if (
+          val.first_name.toLowerCase().includes(searchName.toLowerCase())
+        ) {
+          return val;
+        }
+      }).map((val, key) => {
         return (
-          <div>
+          <div key={key}>
             <p>{val.first_name}</p>
           </div>
         );
